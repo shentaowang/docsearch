41a470e12125c21cf2f39ee8a3e158d7e2bf26f6
CenU410UQ <18362927193@163.com>
2017-03-23 15:56:48 +0800

    the first index disturbition

--- OperateEs.py
+++ OperateEs.py
@@ -8,35 +8,51 @@ class Operate(object):
 		self.port = port
 		self.username = username
 		self.es = Elasticsearch([{'host':host,'port':port}])
- 
-	def create_index(self):
-		self.es.indices.create(index = self.username)
 	
-	def mapping(self, doc_class):
-		map_body = {
-				"mappings":{
-					"properties":{
-						"content":{
-							"type":"text",
-							"analyzer":"ik_max_word",
-							"include_in_all":"true",
-							"bost":8
+	def initial(self):
+		if self.es.indices.exists(index=self.username):
+			self.es.indices.delete(index=self.username)
+			map_body = {
+					"mappings":{
+						"word":{
+							"_all":{
+								"analyzer":"ik_max_word",
+								"search_analyzer":"ik_max_word",
+								"term_vector":"no",
+								"store":"false"
+								},
+							"properties":{
+								"doc_name":{
+									"type":"text",
+									"analyzer":"ik_max_word",
+									"include_in_all":"true",
+									"boost":8
+									},
+								"path":{
+									"type":"text",
+									"analyzer":"ik_max_word",
+									"include_in_all":"true",
+									"boost":8
+									},
+								"content":{
+									"type":"text",
+									"analyzer":"ik_max_word",
+									"include_in_all":"true",
+									"boost":8
+									},
+								"importance":{
+									"type":"integer"
+									},
+								"insert_time":{
+									"type":"date",
+									"format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+									},
+								"remark":{
+									"type":"text"
+									}
+								}
 							}
 						}
 					}
-				
-				}
-		self.es.index(index = self.username,doc_type = doc_class,body = map_body)
-	
-	def insert_doc(self, content):
-		insert_body = {
-				
-				}
-	
-	def query_settings(self, index_name):
-		print self.es.indices.get_settings(index = index_name)
+			self.es.indices.create(index=self.username,body=map_body)
 
-
-	def simple_search(self, username):
-		#return self.es.get("index" = self.username)['_source']
-		pass
--- main.py
+++ main.py
@@ -9,83 +9,8 @@ HOST = "127.0.0.1"
 PORT = 9200
 USERNAME = 'light'
 
-es = Elasticsearch([{"host":HOST, "port":PORT}])
-#delete the index
-#print es.indices.delete(index=USERNAME)
+main_es = Elasticsearch([{"host":HOST, "port":PORT}])
+class_es = OperateEs.Operate(HOST, PORT, USERNAME)
+#class_es.initial()
 
-#create the index 
-#doc_index = es.indices.create(index=USERNAME)
-#print doc_index
-
-
-#create the mapping
-map_body = {
-		"settings":{
-			"number_of_shards":1
-			},
-		"mappings":{
-			"pdf":{
-				"_all":{
-					"analyzer":"ik_max_word",
-					"search_analyzer":"ik_max_word",
-					"term_vector":"no",
-					"store":"false"
-					},
-				"properties":{
-					"content":{
-						"type":"text",
-						"analyzer":"ik_max_word",
-						"include_in_all":"true",
-						"boost":8
-						}
-					}
-				}
-			}
-		}
-#doc_map = es.indices.create(index=USERNAME, body=map_body)
-#print doc_map
-
-
-#insert the data
-insert_body = {
-		"content":"特朗普上台后中国的日子不好过啊！"
-		}
-doc_insert = es.create(index=USERNAME, doc_type="pdf", id=3, body=insert_body)
-print doc_insert
-
-
-#query the data
-search_body = {
-		"query":{
-			"match":{
-				"content":"中国和美国"
-				}
-			},
-		"highlight":{
-			"pre_tags":["<tag1>", "<tag2>"],
-			"post_tags":["</tag1>", "</tag2>"],
-			"fields":{
-				"content":{}
-				}
-			}
-		}
-doc_search = es.search(index=USERNAME, body=search_body)
-print doc_search
-
-
-#get the mapping
-doc_mapping = es.indices.get_mapping(index=USERNAME)
-print doc_mapping
-
-
-#delete the index
-#print es.indices.delete(index=USERNAME)
-
-
-read = GetContent.SimpRead()
-
-content  = read.read_docx(filename)
-for line in content:
-	print line.text
-
-
+print main_es.indices.get_mapping(index=USERNAME)
