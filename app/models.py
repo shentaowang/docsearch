7b2b68064bd14a03fa2ec7c14e7875f2305a0305
CenU410UQ <18362927193@163.com>
2017-04-08 09:34:32 +0800

    准备用于登陆的用户模型

--- app/models.py
+++ app/models.py
@@ -6,11 +6,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from flask import request
 from app import db
+from . import login_manager
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
@@ -30,5 +32,9 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     user = db.relationship('User', backref='role')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
